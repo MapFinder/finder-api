e030eeb9d0cd2553885edb2b27bbff89b65b3171
Andrey Ryabko <honestua@gmail.com>
2017-05-26 17:28:25 +0300

    Render json from object

--- src/main/java/finder/controllers/LostItemsController.java
+++ src/main/java/finder/controllers/LostItemsController.java
@@ -1,19 +1,21 @@
 package finder.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import finder.model.Item;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @Controller
 @CrossOrigin
 public class LostItemsController {
-
     @RequestMapping("/lost-items")
-    @ResponseBody
-    public ResponseEntity getLostItems() {
-        return new ResponseEntity("{ items: [] }", HttpStatus.OK);
+    public @ResponseBody Item getLostItems() {
+        Item item = new Item("Some description");
+
+        return item;
     }
 }
---
+++ src/main/java/finder/model/Item.java
@@ -0,0 +1,17 @@
+package finder.model;
+
+public class Item {
+    private String description;
+
+    public Item(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
